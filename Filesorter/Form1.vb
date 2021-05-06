362744c4d376be7210e0423cb08bd1b525954101
luke1hl <51419088+luke1hl@users.noreply.github.com>
2021-05-06 11:36:39 +0100

    Update Form1.vb

--- Filesorter/Form1.vb
+++ Filesorter/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim numberarray() As Array
+        Dim numberarray() As Integer
 
         Dim filepath As String
         Select Case OpenFileDialog1.ShowDialog()
