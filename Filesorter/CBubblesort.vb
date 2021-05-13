8bf385ba91e5b52154ac7e7d00dadeff1a550ea9
ManicJamie <30422476+ManicJamie@users.noreply.github.com>
2021-05-13 21:33:30 +0100

    Fix 2
    Use Int64 to prevent overflow, do an extra pass on bubble sort to ensure sorted

--- Filesorter/CBubblesort.vb
+++ Filesorter/CBubblesort.vb
@@ -1,15 +1,15 @@
 ﻿Public Class CBubblesort
     Structure values
-        Dim count As Integer
-        Dim swaps As Integer
+        Dim count As Int64
+        Dim swaps As Int64
     End Structure
-    Function BubbleSort(ByVal Numlist As Object) As values
+    Function BubbleSort(ByVal Numlist() As Integer) As values
         Dim valuesstructure As New values
 
         valuesstructure.swaps = 0
         valuesstructure.count = 0
         Dim Swapvalue As Integer
-        For ii = 0 To Numlist.Length - 2
+        For ii = 0 To Numlist.Length - 1
             For i = 0 To Numlist.Length - 2
                 valuesstructure.count += 1
 
